fa51956b620bda8136438125b8c969f9c9446d05
Mikael Medeiros <mikaeltruth@gmail.com>
2021-04-21 12:54:07 -0300

    Configuração do primeng para o tema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SahredModule } from './shared/sahred/sahred.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SahredModule
   ],
   providers: [],
   bootstrap: [AppComponent]
